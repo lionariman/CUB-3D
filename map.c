ac14698dd4983339639323d7fdaa4a1af37e6206
lionariman <lionariman@gmail.com>
2021-01-28 23:28:24 +0300

    map parser almost on finish

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: keuclide <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 20:37:40 by keuclide          #+#    #+#             */
-/*   Updated: 2021/01/28 18:20:57 by keuclide         ###   ########.fr       */
+/*   Updated: 2021/01/28 23:18:12 by keuclide         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,16 @@ int		check_arg(char *s, char c)
 		if (s[i] == c)
 			return (0);
 	return (-1);
+}
+
+int		skiplines(char **lines)
+{
+	int i;
+
+	i = 0;
+	while (lines[i])
+		i++;
+	return (i - 1);
 }
 
 void	player_pos(t_all *all, int i, int j)
@@ -58,16 +68,25 @@ int		check_map(t_all *all)
 	return ((all->flags.p_flag > 1 || !all->flags.p_flag) ? -1 : 0);
 }
 
-//---------------------------<parse-map>------------------------------
 
-int		skiplines(char **lines)
+int		check_around(t_all *all, int i, int j)
 {
-	int i;
-
-	i = 0;
-	while (lines[i])
-		i++;
-	return (i - 1);
+	j++;
+	while (all->map[i][j] && j < ft_strlen(all->map[i]) - 1)
+	{
+		if (!check_arg("02NWES", all->map[i][j]))
+			if (check_arg("012NWES", all->map[i][j + 1]) == -1 ||
+				check_arg("012NWES", all->map[i][j - 1]) == -1 ||
+				check_arg("012NWES", all->map[i - 1][j]) == -1 ||
+				check_arg("012NWES", all->map[i + 1][j]) == -1 ||
+				check_arg("012NWES", all->map[i + 1][j + 1]) == -1 ||
+				check_arg("012NWES", all->map[i + 1][j - 1]) == -1 ||
+				check_arg("012NWES", all->map[i - 1][j + 1]) == -1 ||
+				check_arg("012NWES", all->map[i - 1][j - 1]) == -1)
+				return (-1);
+		j++;
+	}
+	return (0);
 }
 
 int		parse_map(t_all *all)
@@ -85,6 +104,8 @@ int		parse_map(t_all *all)
 		j = skipspaces(all->map[i]);
 		if (!check_arg("02NWES", all->map[i][j]))
 			return (-1);
+		if (check_around(all, i, j) == -1)
+			return (print_error("shit"));
 		j = ft_strlen(all->map[i]) - 1;
 		if (!check_arg("02NWES", all->map[i][j]))
 			return (-1);
@@ -97,20 +118,28 @@ int		parse_map(t_all *all)
 	return (0);
 }
 
-//---------------------------<parse-map>------------------------------
 
 int		store_map(t_list **head, t_all *all, int size)
 {
 	t_list	*tmp;
 	int		i;
+	int		f;
 
 	i = 0;
+	f = 0;
 	tmp = *head;
 	if (!(all->map = ft_calloc(size + 1, sizeof(char *))))
 		return (-1);
-	while (tmp)
+	while (tmp && size > 0)
 	{
-		all->map[i++] = ft_strdup(tmp->content);
+		if (ft_strlen(tmp->content))
+		{
+			all->map[i++] = ft_strdup(tmp->content);
+			size--;
+			f = 1;
+		}
+		if (!ft_strlen(tmp->content) && f == 1)
+			return (print_error("empty lines..."));
 		free(tmp->content);
 		tmp = tmp->next;
 	}
@@ -122,17 +151,20 @@ int		store_map(t_list **head, t_all *all, int size)
 int		read_map(int fd, char *line, t_all *all)
 {
 	t_list	*head;
+	int		i;
 
+	i = 0;
 	head = NULL;
+	line[0] != '\0' ? (i++) : 0;
 	ft_lstadd_back(&head, ft_lstnew(line));
 	while (get_next_line(fd, &line) > 0)
+	{
+		line[0] != '\0' ? (i++) : 0;
 		ft_lstadd_back(&head, ft_lstnew(line));
+	}
+	line[0] != '\0' ? (i++) : 0;
 	ft_lstadd_back(&head, ft_lstnew(line));
-	// line[0] != '\0' ? ft_lstadd_back(&head, ft_lstnew(line)) : free(line);
-	// while (get_next_line(fd, &line) > 0)
-	// 	line[0] != '\0' ? ft_lstadd_back(&head, ft_lstnew(line)) : free(line);
-	// line[0] != '\0' ? ft_lstadd_back(&head, ft_lstnew(line)) : free(line);
-	if (store_map(&head, all, ft_lstsize(head)) == -1)
+	if (store_map(&head, all, i) == -1)
 		return (print_error("malloc error"));
 	if (check_map(all) == -1)
 		return (print_error("map error"));
--- x.sh
+++ x.sh
@@ -2,6 +2,6 @@ make clean
 cp -R ~/cursus/cub3d/* ~/cursus/cub
 cd ../cub
 git add .
-git commit -m "map parser have been added"
+git commit -m "map parser almost on finish"
 git push origin main
 cd ../cub3d
