64410efc297d9b9a488662a4c1109bc74d76eb31
lionariman <lionariman@gmail.com>
2021-01-29 21:01:37 +0300

    new changes have been added

--- raycast.c
+++ raycast.c
@@ -6,19 +6,12 @@
 /*   By: keuclide <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 13:53:09 by keuclide          #+#    #+#             */
-/*   Updated: 2021/01/29 15:59:22 by keuclide         ###   ########.fr       */
+/*   Updated: 2021/01/29 20:24:01 by keuclide         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-// void	my_mlx_pixel_put(t_wndw *data, int x, int y, int color)
-// {
-// 	char	*dst;
-
-// 	dst = data->addr + (y * data->line_len + x * (data->bppixel / 8));
-// 	*(unsigned int *)dst = color;
-// }
 
 // int		key_hook(int keycode, t_wndw *win)
 // {
@@ -27,65 +20,62 @@
 // 	return (0);
 // }
 
-// void	scale_map(t_all all)
-// {
-// 	t_point end;
+void	my_mlx_pixel_put(t_wndw *data, int x, int y, int color)
+{
+	char	*dst;
 
-// 	end.x = (all.point.x + 1) * SCALE;
-// 	end.y = (all.point.y + 1) * SCALE;
-// 	all.point.x *= SCALE;
-// 	all.point.y *= SCALE;
-// 	while (all.point.y < end.y)
-// 	{
-// 		while (all.point.x < end.x)
-// 		{
-// 			mlx_pixel_put(all.win.mlx, all.win.win, all.point.x, all.point.y, 0x0F0FFF);
-// 			all.point.x++;
-// 		}
-// 		all.point.x -= SCALE;
-// 		all.point.y++;
-// 	}
-// }
+	dst = data->addr + (y * data->line_len + x * (data->bppixel / 8));
+	*(unsigned int *)dst = color;
+}
 
-// void	draw_map(t_all *all)
-// {
-// 	all->win.mlx = mlx_init();
-// 	all->win.win = mlx_new_window(all->win.mlx, all->res.x, all->res.y, "Wolfenstein");
+void	scale_map(t_all all, int i, int j)
+{
+	int x;
+	int y;
 
-// 	all->point.y = 0;
-// 	while (all->map[all->point.y])
-// 	{
-// 		all->point.x = 0;
-// 		while (all->map[all->point.y][all->point.x])
-// 		{
-// 			if (all->map[all->point.y][all->point.x] == '1')
-// 				scale_map(*all);
-// 			all->point.x++;
-// 		}
-// 		all->point.y++;
-// 	}
-// 	mlx_loop(all->win.mlx);
-// }
+	x = (j + 1) * SCALE;
+	y = (i + 1) * SCALE;
+	j *= SCALE;
+	i *= SCALE;
+	while (i < y)
+	{
+		while (j < x)
+		{
+			my_mlx_pixel_put(&all.win, j, i, all.color.c);
+			// mlx_pixel_put(all.win.mlx, all.win.win, j, i, all.color.c);
+			j++;
+		}
+		j -= SCALE;
+		i++;
+	}
+}
 
 int		raycast(t_all *all)
 {
 	int i;
 	int j;
 
-	i = 0;
+	i = 1;
+	j = 1;
 	all->win.mlx = mlx_init();
 	all->win.win = mlx_new_window(all->win.mlx, all->res.x, all->res.y, "Wolfenstein");
+	all->win.img = mlx_new_image(all->win.mlx, all->res.x, all->res.y);
+	all->win.addr = mlx_get_data_addr(all->win.mlx, &all->win.bppixel,
+	&all->win.line_len, &all->win.endian);
+	// mlx_pixel_put(all->win.mlx, all->win.win, j, i, all->color.c);
+	// my_mlx_pixel_put(&all->win, j, i, all->color.c);
 	while (all->map[i])
 	{
 		j = 0;
 		while (all->map[i][j])
 		{
 			if (all->map[i][j] == '1')
-				mlx_pixel_put(all->win.mlx, all->win.win, j, i, all->color.f);
+				scale_map(*all, i, j);
 			j++;
 		}
 		i++;
 	}
+	mlx_put_image_to_window(all->win.mlx, all->win.win, all->win.img, 0, 0);
 	mlx_loop(all->win.mlx);
 	return (0);
 }
