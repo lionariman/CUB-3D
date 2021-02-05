af688d946c14d9b78b356a76c1c79809ad7fb4a9
lionariman <lionariman@gmail.com>
2021-02-05 18:01:07 +0300

    new changes

--- x.sh
+++ x.sh
@@ -1,8 +1,8 @@
 make clean
 cp -R ~/cursus/cub3d/* ~/cursus/cub
+rm -rf ~/cursus/cub/a.out.dSYM ~/cursus/cub/a.out
 cd ../cub
 git add .
 git commit -m "new changes"
 git push origin main
 cd ../cub3d
-rm -rf ~/cursus/cub/a.out.dSYM ~/cursus/cub/a.out
