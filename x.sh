297f4a873b3df2f03e7f7191892e5825fbea99c1
lionariman <lionariman@gmail.com>
2021-02-06 22:15:11 +0300

    new changes

--- x.sh
+++ x.sh
@@ -3,6 +3,6 @@ cp -R ~/cursus/cub3d/* ~/cursus/cub
 rm -rf ~/cursus/cub/a.out.dSYM ~/cursus/cub/a.out
 cd ../cub
 git add .
-git commit -m "new button add"
+git commit -m "new changes"
 git push origin main
 cd ../cub3d
