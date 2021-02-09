4c7700101fd93c5e9aa28f578c2c51b8acfe736a
lionariman <lionariman@gmail.com>
2021-02-10 00:19:49 +0300

    new changes have been added

--- x.sh
+++ x.sh
@@ -3,6 +3,6 @@ cp -R ~/cursus/cub3d/* ~/cursus/cub
 rm -rf ~/cursus/cub/a.out.dSYM ~/cursus/cub/a.out
 cd ../cub
 git add .
-git commit -m "sprites bugs have been fixed"
+git commit -m "new changes have been added"
 git push origin main
 cd ../cub3d
