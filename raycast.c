11b506a2df73f176e1e85f5594a9ba9af4203cad
lionariman <lionariman@gmail.com>
2021-02-10 17:40:12 +0300

    new changes have been added

--- raycast.c
+++ raycast.c
@@ -6,7 +6,7 @@
 /*   By: keuclide <keuclide@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 13:53:09 by keuclide          #+#    #+#             */
-/*   Updated: 2021/02/10 17:36:03 by keuclide         ###   ########.fr       */
+/*   Updated: 2021/02/10 17:40:04 by keuclide         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -221,15 +221,15 @@ int		cub(t_all *l)
 		l->draw_end = l->l_height / 2 + l->res.y / 2;
 		(l->draw_end >= l->res.y) ? (l->draw_end = l->res.y - 1) : 0;
 
-		// l->sd == 0 ?
-		// (l->wall_x = l->plr.pos_y + l->p_wall_d * l->ray.dir_y) :
-		// (l->wall_x = l->plr.pos_x + l->p_wall_d * l->ray.dir_x);
-		// l->wall_x -= floor(l->wall_x);
-		// l->tex_x = (int)(l->wall_x * (double)l->w);
-		// l->tex_x = l->w - l->tex_x - 1;
+		l->sd == 0 ?
+		(l->wall_x = l->plr.pos_y + l->p_wall_d * l->ray.dir_y) :
+		(l->wall_x = l->plr.pos_x + l->p_wall_d * l->ray.dir_x);
+		l->wall_x -= floor(l->wall_x);
+		l->tex_x = (int)(l->wall_x * (double)l->w);
+		l->tex_x = l->w - l->tex_x - 1;
 		
-		// l->zstep = 1.0 * l->h / l->l_height;
-		// l->tex_pos = (l->draw_start - l->res.y / 2 + l->l_height / 2) * l->zstep;
+		l->zstep = 1.0 * l->h / l->l_height;
+		l->tex_pos = (l->draw_start - l->res.y / 2 + l->l_height / 2) * l->zstep;
 
 		i = 0;
 		while (i < l->res.y)
@@ -238,17 +238,17 @@ int		cub(t_all *l)
 				my_mlx_pixel_put(&l->win, x, i, l->color.c);
 			else if (i >= l->draw_start && i <= l->draw_end)
 			{
-				// l->tex_y = (int)l->tex_pos & (l->h - 1);
-				// l->tex_pos += l->zstep;
-				// if (l->sd == 0)
-				// 	(l->step.x > 0) ?
-				// 	(l->rgb = pixget(&l->tx[0], l->tex_x, l->tex_y)) :
-				// 	(l->rgb = pixget(&l->tx[1], l->tex_x, l->tex_y));
-				// else
-				// 	(l->step.y > 0) ?
-				// 	(l->rgb = pixget(&l->tx[2], l->tex_x, l->tex_y)) :
-				// 	(l->rgb = pixget(&l->tx[3], l->tex_x, l->tex_y));
-				my_mlx_pixel_put(&l->win, x, i, l->rgb = 0xFFFFFF);
+				l->tex_y = (int)l->tex_pos & (l->h - 1);
+				l->tex_pos += l->zstep;
+				if (l->sd == 0)
+					(l->step.x > 0) ?
+					(l->rgb = pixget(&l->tx[0], l->tex_x, l->tex_y)) :
+					(l->rgb = pixget(&l->tx[1], l->tex_x, l->tex_y));
+				else
+					(l->step.y > 0) ?
+					(l->rgb = pixget(&l->tx[2], l->tex_x, l->tex_y)) :
+					(l->rgb = pixget(&l->tx[3], l->tex_x, l->tex_y));
+				my_mlx_pixel_put(&l->win, x, i, l->rgb);
 			}
 			if (i <= l->res.y && i >= l->draw_end)
 				my_mlx_pixel_put(&l->win, x, i, l->color.f);
