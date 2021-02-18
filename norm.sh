e30eb4b85a948336e09dc21569d876dadb74f048
lionariman <lionariman@gmail.com>
2021-02-18 14:14:29 +0300

    add png file and new map

--- norm.sh
+++
@@ -1,7 +0,0 @@
-norminette parser/*
-norminette engine/*
-norminette libft/*
-norminette movement/*
-norminette tools/*
-norminette bitmap/*
-norminette header/*
--- x.sh
+++
@@ -1,8 +0,0 @@
-make clean
-cp -R ~/cursus/cub3d/* ~/cursus/cub
-rm -rf ~/cursus/cub/a.out.dSYM ~/cursus/cub/a.out
-cd ../cub
-git add .
-git commit -m "All bugs have been fixed. The end."
-git push origin main
-cd ../cub3d
